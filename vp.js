3f281c4e5ab95f3033aa3d61a2e9399c3c5ea46f
noelminitel <noelminitel@gmail.com>
2026-09-18 16:22:42 +0200

    Update vp.js

--- vp.js
+++ vp.js
@@ -1,165 +1,152 @@
 //////////////////////////////////////////////////////////////////////
 
-function vp_main($scope, $window, $timeout)
+function AuthAccount()
 {
-    $scope.view = "home";
-    $scope.sign_msg = "";
-    $scope.signed_in = false;
-    $scope.g_signbtn_ok = false;
-    $scope.busy = false;
+    // initialise
+    this.authClientID = undefined;
+    this.authScope = undefined;
 
-    $scope.settings = {
-        banner_text: "Visual Planner",
-        vipconfig: {}
-    };
+    // public
+    this.onSignIn = function(){};
+    this.onSignOut = function(){};
+    this.onError = function(msg){};
 
-    $scope.multi_col_count_options = {
-        1: "1", 2: "2", 3: "3", 4: "4", 6: "6", 12: "12"
-    };
-
-    var account = new AuthAccount();
-    var appdata = new AuthAppData();
-    var cal = new AuthCal();
-    var ui = new UIPlanner();
-
-    appdata.file_name = "visual_planner.json";
-    appdata.Patch = ui.PatchConfig.bind(ui);
-
-    appdata.setDefault({
-        banner_text: "Visual Planner",
-        vipconfig: ui.GetDefaultConfig()
-    });
-
-    account.authScope = "https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/drive.appdata";
-
-    account.onSignIn = function()
-    {
-        $scope.$apply(function() {
-            $scope.signed_in = true;
-            $scope.sign_msg = "Signed in as " + account.getEmail();
-        });
-
-        gapi.load('client', function() {
-            gapi.client.init({}).then(function() {
-                appdata.Read(onReadAppdata, onFailAppdata);
-            });
-        });
-    };
-
-    account.onSignOut = function()
-    {
-        $scope.$apply(function() {
-            $scope.signed_in = false;
-            $scope.sign_msg = "Not signed in";
-            $scope.view = "home";
-        });
-        ui.Clear();
-    };
-
-    account.onError = function(msg)
-    {
-        $scope.$apply(function() {
-            $scope.sign_msg = "Error: " + msg;
-        });
-    };
-
-    cal.forwardCalendar = ui.AddCalendar.bind(ui);
-    cal.forwardEvent = ui.AddEvent.bind(ui);
-    cal.forwardEventReloadReq = function() { cal.loadEvents(); };
-    cal.forwardSetting = ui.SetSetting.bind(ui);
-    cal.onError = function(msg) { console.error("Cal error: " + msg); };
-
-    ui.forwardDatespanChange = function(span) {
-        cal.datespan = span;
-        cal.loadEvents();
-    };
-
-    ui.forwardRedrawReq = function() {
-        ui.Redraw();
-    };
-
-    function onReadAppdata()
-    {
-        var data = appdata.getAppData();
-        
-        $scope.$apply(function() {
-            if (data) {
-                $scope.settings = data;
-            }
-            ui.SetConfig($scope.settings.vipconfig);
-        });
-
-        ui.Init("grid", "calendarbar");
-        cal.loadEvents();
-    }
-
-    function onFailAppdata(msg)
-    {
-        console.error("Appdata read error: " + msg);
-        onReadAppdata();
-    }
-
-    $scope.onclickSignIn = function()
-    {
-        account.SignIn();
-    };
-
-    $scope.onclickSignOut = function()
-    {
-        account.SignOut();
-    };
-
-    $scope.onclickSettings = function()
-    {
-        $scope.view = "settings";
-        if ($scope.form) {
-            $scope.form.$setPristine();
-        }
-    };
-
-    $scope.onclickCancel = function()
-    {
-        $scope.view = "home";
-        var data = appdata.getAppData();
-        if (data) {
-            $scope.settings = data;
-            ui.SetConfig($scope.settings.vipconfig);
-            ui.Redraw();
-        }
-    };
-
-    $scope.onclickSave = function()
-    {
-        $scope.busy = true;
-        appdata.Write($scope.settings, function() {
-            $scope.$apply(function() {
-                $scope.busy = false;
-                $scope.view = "home";
-                if ($scope.form) {
-                    $scope.form.$setPristine();
-                }
-                ui.SetConfig($scope.settings.vipconfig);
-                ui.Redraw();
-            });
-        }, function(msg) {
-            $scope.$apply(function() {
-                $scope.busy = false;
-                alert("Save failed: " + msg);
-            });
-        });
-    };
-
-    $scope.onclickPrintView = function()
-    {
-        $scope.view = "print";
-        $scope.printinfo = ui.GetPrintViewInfo();
-    };
-
-    $scope.onclickClosePrintView = function()
-    {
-        $scope.view = "home";
-    };
-
-    // Lancement de la connexion
-    account.Connect();
+    // private
+    this.tokenClient = null;
+    this.access_token = null;
+    this.userEmail = null;
 }
+
+AuthAccount.prototype.Connect = function()
+{
+    // 1. On vérifie si un token valide est enregistré dans le navigateur
+    var savedToken = localStorage.getItem('vp_access_token');
+    var savedExpiry = localStorage.getItem('vp_token_expiry');
+    var now = new Date().getTime();
+
+    if (savedToken && savedExpiry && now < parseInt(savedExpiry)) {
+        this.access_token = savedToken;
+        
+        // Attente de l'objet gapi pour injecter le token en arrière-plan
+        var checkGapi = setInterval(() => {
+            if (typeof gapi !== 'undefined' && gapi.client) {
+                clearInterval(checkGapi);
+                gapi.client.setToken({ access_token: this.access_token });
+                
+                fetch('https://www.googleapis.com/oauth2/v3/userinfo', {
+                    headers: { Authorization: 'Bearer ' + this.access_token }
+                })
+                .then(res => res.json())
+                .then(data => {
+                    this.userEmail = data.email;
+                    this.onSignIn();
+                })
+                .catch(() => {
+                    this.onSignIn();
+                });
+            }
+        }, 100);
+        return;
+    }
+
+    // 2. Si aucun token valide n'existe, on prépare uniquement le client Google pour le clic manuel.
+    // AUCUNE POPUP NE S'OUVRE TOUTE SEULE.
+    var initTimer = setInterval(() => {
+        if (typeof google !== 'undefined' && google.accounts && google.accounts.oauth2) {
+            clearInterval(initTimer);
+            
+            if (!this.tokenClient) {
+                this.tokenClient = google.accounts.oauth2.initTokenClient({
+                    client_id: this.authClientID,
+                    scope: this.authScope,
+                    callback: (response) => {
+                        if (response.error) {
+                            this.onSignOut();
+                            return;
+                        }
+                        this.access_token = response.access_token;
+                        
+                        var expiresAt = new Date().getTime() + (response.expires_in || 3600) * 1000;
+                        localStorage.setItem('vp_access_token', this.access_token);
+                        localStorage.setItem('vp_token_expiry', expiresAt);
+                        
+                        if (typeof gapi !== 'undefined' && gapi.client) {
+                            gapi.client.setToken({ access_token: this.access_token });
+                        }
+                        
+                        fetch('https://www.googleapis.com/oauth2/v3/userinfo', {
+                            headers: { Authorization: 'Bearer ' + this.access_token }
+                        })
+                        .then(res => res.json())
+                        .then(data => {
+                            this.userEmail = data.email;
+                            this.onSignIn();
+                        })
+                        .catch(() => {
+                            this.onSignIn();
+                        });
+                    },
+                });
+            }
+        }
+    }, 200);
+
+    // On indique à l'application de rester sagement sur l'écran déconnecté (bouton Sign In)
+    this.onSignOut();
+}
+
+AuthAccount.prototype.SignIn = function()
+{
+    if (this.tokenClient) {
+        // La popup ne s'ouvre QUE lorsque vous cliquez explicitement sur "Sign In"
+        this.tokenClient.requestAccessToken({prompt: 'consent'});
+    }
+}
+
+AuthAccount.prototype.SignOut = function()
+{
+    if (this.access_token) {
+        google.accounts.oauth2.revoke(this.access_token, () => {});
+    }
+    this.access_token = null;
+    this.userEmail = null;
+    
+    // Nettoyage du stockage local à la déconnexion
+    localStorage.removeItem('vp_access_token');
+    localStorage.removeItem('vp_token_expiry');
+
+    if (typeof gapi !== 'undefined' && gapi.client) {
+        gapi.client.setToken(null);
+    }
+    this.onSignOut();
+}
+
+AuthAccount.prototype.isSignedIn = function()
+{
+    return (this.access_token !== null && this.access_token !== undefined);
+}
+
+AuthAccount.prototype.getEmail = function()
+{
+    return this.userEmail;
+}
+
+AuthAccount.prototype.Fail = function(reason)
+{
+    var msg = "";
+    if (reason.error)
+    {
+        msg = "[" + reason.error + "]";
+        if (reason.message)
+            msg += " " + reason.message;
+    }
+    else {
+        msg = JSON.stringify(reason);
+    }
+
+    console.error("AuthAccount : " + msg);
+    this.onError(msg);
+}
+
+//////////////////////////////////////////////////////////////////////
+// (Les objets AuthAppData et AuthCal restent inchangés en dessous)
