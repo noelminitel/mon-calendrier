3965f0f14208ea8d79fe797907f815c3aaa1ed48
noelminitel <noelminitel@gmail.com>
2026-09-18 16:28:09 +0200

    Update vp_config.js

--- vp_config.js
+++ vp_config.js
@@ -1,7 +1,7 @@
-vp_oauthClientID = '749475982945-me64prs1nn6og0ghqj7ifgc4c6js71ta.apps.googleusercontent.com';  // vp
-vp_unauthAPIKey = "AIzaSyAHbw39l4a3ZHosnTKGmMpVkKEozKnBVMU";  // vp
+var g_client_id = '749475982945-me64prs1nn6og0ghqj7ifgc4c6js71ta.apps.googleusercontent.com';
+var g_api_key = 'AIzaSyAHbw39l4a3ZHosnTKGmMpVkKEozKnBVMU';
 
-// google analytics
+// Google Analytics (conservez cette partie si vous l'avez)
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
